072ea8ca908dcbc37f449c3d580c18b73f03f67c
Batuhan güngör <batuhan@jointventures.io>
2018-01-18 01:35:25 +0300

    ekranlar

---
+++ AtesBocegi.App/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtesBocegi.App.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View(db.Article.Where(q=> q.PageName == "Ana Sayfa").Include(q=> q.Color).OrderBy(q=> q.ScreenOrder).ToList());
+        }
+    }
+}
